042a8165d682c81b79b2a9a3cef892dcf329c2da
TupikovES <evgeniy.tupikov@gmail.com>
2015-11-02 00:28:20 +0400

    add hex numeral system

--- src/com/samgtu/cons/Conv.java
+++ src/com/samgtu/cons/Conv.java
@@ -21,6 +21,7 @@ public class Conv extends JFrame{
     private JButton exit;
     private JLabel textIn;
     private JLabel textOut;
+    private JLabel errMess;
 
     private ActionListener exitAction = new ActionListener() {
         @Override
@@ -37,27 +38,38 @@ public class Conv extends JFrame{
 
         @Override
         public void keyPressed(KeyEvent e) {
-
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
-            int num = Integer.parseInt(inData.getText());
-            Integer max = num % 10;
-            for (int i = num; i > 0; i /= 10) {
-                int next = i % 10;
-                if (max < next)
-                    max = next;
-            }
-            numeralIn.removeAllItems();
-            for (int i = max + 1; i <= 10; i++) {
-                numeralIn.addItem(i);
+            int num = (int)inData.getText().toCharArray()[inData.getText().length() - 1];
+            int max = 0;
+            if ((num >= 48 && num <= 57) || (num >= 65 && num <= 70)) {
+                for (int i = 0; i < inData.getText().length(); i++) {
+                    int next = 0;
+                    if (num < 58)
+                        next = (int)inData.getText().toCharArray()[i] - 48;
+                    else
+                        next = (int)inData.getText().toCharArray()[i] - 55;
+                    if (max < next)
+                        max = next;
+                }
+
+                numeralIn.removeAllItems();
+                for (int i = max + 1; i <= 16; i++) {
+                    numeralIn.addItem(i);
+                }
+                errMess.setText("");
+            } else {
+                numeralIn.removeAllItems();
+                errMess.setText("Неверные данные");
             }
         }
     };
     private ActionListener rangeNumeral = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
+            /*
             int num = Integer.parseInt(inData.getText());
             Integer max = num % 10;
             for (int i = num; i > 0; i /= 10) {
@@ -69,16 +81,26 @@ public class Conv extends JFrame{
             for (int i = max + 1; i <= 10; i++) {
                 numeralIn.addItem(i);
             }
+            */
+
+            String bufS = inData.getText();
+            inData.setText(bufS.toUpperCase());
+
+            //inData.setText(inData.getText().toUpperCase());
         }
     };
 
     private ActionListener convertNum = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
+            /*
             String t;
             int next = toDec(Integer.parseInt(inData.getText()), (int)numeralIn.getSelectedItem());
             t = "" + fromDec(next, (int)numeralOut.getSelectedItem());
             outData.setText(t);
+            */
+
+            outData.setText(fromDecNew(toDecNew(inData.getText(), (int)numeralIn.getSelectedItem()), (int)numeralOut.getSelectedItem()));
         }
     };
 
@@ -87,8 +109,8 @@ public class Conv extends JFrame{
         inData.addActionListener(rangeNumeral);
         inData.addKeyListener(re);
         convert.addActionListener(convertNum);
-        inData.setText("0");
-        for (int i = 2; i <= 10; i++) {
+        //inData.setText("0");
+        for (int i = 2; i <= 16; i++) {
             numeralOut.addItem(i);
         }
         setContentPane(rootPanel);
--- src/com/samgtu/cons/util/ConvertMethod.java
+++ src/com/samgtu/cons/util/ConvertMethod.java
@@ -1,5 +1,7 @@
 package com.samgtu.cons.util;
 
+import java.math.BigInteger;
+
 /**
  * Created by x3mib_000 on 30.10.2015.
  */
@@ -35,4 +37,58 @@ public class ConvertMethod {
         return s;
     }
 
+    public static String toDecNew(String s, int ns) {
+        int[] num = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            int buf = (int)s.charAt(i);
+            if (buf < 58)
+                num[i] = buf - 48;
+            else
+                num[i] = buf - 55;
+        }
+
+        //BigInteger sum = new BigInteger("0");
+        long sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            sum += Math.pow(ns, i) * num[s.length() - i - 1];
+            //sum.add(BigInteger.valueOf((long)Math.pow(ns, i)).multiply(BigInteger.valueOf(num[s.length() - i - 1])));
+        }
+        return String.valueOf(sum);
+    }
+
+    public static String fromDecNew(String s, int ns) {
+        BigInteger
+                b = new BigInteger(s),
+                nsBig = BigInteger.valueOf(ns);
+
+        BigInteger[] bigArr;
+        String value = "";
+        char val;
+        while (true) {
+            bigArr = b.divideAndRemainder(nsBig);
+            if(bigArr[1].compareTo(BigInteger.valueOf(10)) < 0) {
+                val = (char)(Integer.parseInt(bigArr[1].toString()) + 48);
+            }
+            else {
+                val = (char)(Integer.parseInt(bigArr[1].toString()) + 55);
+            }
+            value = val + value;
+            System.out.println(val);
+            if ((bigArr[0].compareTo(nsBig)) < 0) {
+                if(bigArr[0].compareTo(BigInteger.valueOf(10)) < 0) {
+                    val = (char)(Integer.parseInt(bigArr[0].toString()) + 48);
+                }
+                else {
+                    val = (char)(Integer.parseInt(bigArr[0].toString()) + 55);
+                }
+                if (val != '0')
+                    value = val + value;
+                break;
+            }
+            b = bigArr[0];
+        }
+
+        return value;
+    }
 }
